5699b7ebbc3147dd6925d80dcdcd009e8da0afa1
aavila89 <90415322+aavila89@users.noreply.github.com>
2023-08-28 17:51:23 -0400

    Update capacitor.config.ts
    fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.inappbrowser.cap',
   appName: 'base',
   webDir: 'www',
   server: {
